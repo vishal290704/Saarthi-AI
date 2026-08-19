7f32579e7fdafa5b7b78a049214c2c917eb2dd2a
Vishal290704 <vsjee2002@gmail.com>
2026-08-19 12:43:03 +0530

    Folder created in service/auth

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import proxy from "express-http-proxy";
 dotenv.config()
 const app = express() 
 
@@ -8,6 +9,8 @@ const PORT = process.env.PORT || 6000
 app.get("/", (req,res)=>{
     res.send("Hello from Gateway")
 })
+
+app.use("/api/auth", proxy(process.env.AUTH_SERVICE_URL))
 
 app.listen(PORT, ()=>{
     console.log(`Gateway started ${PORT}`)
---
+++ backend/services/auth/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config()
+const app = express() 
+
+const PORT = process.env.PORT || 6001
+
+app.get("/", (req,res)=>{
+    res.send("Hello from Auth-service")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Auth-Service started ${PORT}`)
+})
