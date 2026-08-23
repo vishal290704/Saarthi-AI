1a96930290e7a9a4b8eceb447f247c2cddf552e9
Vishal290704 <vsjee2002@gmail.com>
2026-08-23 19:02:38 +0530

    User checking and creation from firebaseUid

---
+++ backend/services/auth/configs/firebase.js
@@ -0,0 +1,6 @@
+import {cert, initializeApp} from "firebase-admin/app"
+import {serviceAccount} from "../serviceAccountKey.json" with {type:"json"};
+
+export const app = initializeApp({
+    credential: cert(serviceAccount),
+})
--- backend/services/auth/controllers/auth.controller.js
+++ backend/services/auth/controllers/auth.controller.js
@@ -1,7 +1,24 @@
+import { app } from "../configs/firebase.js";
+import {getAuth} from 'firebase-admin/auth'
+import User from "../models/user.model.js";
+
 export const GoogleAuth = async(req, res)=>{
     try {
         const {token} = req.body;
         
+        const decoded = await getAuth(app).verifyIdToken(token)
+
+        let user = await User.findOne({
+            firebaseUid:decoded.uid
+        })
+
+        if(!user){
+            user = await User.create({
+                firebaseUid:decoded.uid,
+                name:decoded.name,
+                email:decoded.email
+            })
+        }
 
     } catch (error) {
         
